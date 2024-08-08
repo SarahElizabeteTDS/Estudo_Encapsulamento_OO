65f56983c2cf6780ddad14a4852ba02eb9d50106
SarahElizabeteTDS <130590547+SarahElizabeteTDS@users.noreply.github.com>
2024-08-08 14:21:46 -0300

    Update att1.php

--- att1.php
+++ att1.php
@@ -45,14 +45,53 @@ class Liquidificador
     }
 
     //GETs e SETs
-    public function setCor($cor)
-    {
-        $this->cor = $cor;
-    }
-
+    
     public function getCor()
     {
         return $this->cor;
+    }
+
+    public function setCor($cor): self
+    {
+        $this->cor = $cor;
+
+        return $this;
+    }
+
+    public function getMarca()
+    {
+        return $this->marca;
+    }
+
+    public function setMarca($marca): self
+    {
+        $this->marca = $marca;
+
+        return $this;
+    }
+
+    public function getVoltagem()
+    {
+        return $this->voltagem;
+    }
+
+    public function setVoltagem($voltagem): self
+    {
+        $this->voltagem = $voltagem;
+
+        return $this;
+    }
+
+    public function getCapacidade()
+    {
+        return $this->capacidade;
+    }
+
+    public function setCapacidade($capacidade): self
+    {
+        $this->capacidade = $capacidade;
+
+        return $this;
     }
 }
 
